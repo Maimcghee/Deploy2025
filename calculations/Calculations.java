936ff1069e9417b6bbd677bb6c6b4eb0375e689a
nv-ram <ramgvenu206@gmail.com>
2025-11-07 20:56:12 -0800

    comments for equations

---
+++ calculations/Calculations.java
@@ -0,0 +1,13 @@
+package calculations;
+public class Calculations {
+    public static void main(String[ ] args){
+  // person mass variable*->  int person_m = ;
+  //planet mass variable*->  int planet_m = ;
+  // radius variable* -> r = ;
+  // find g   gnu = multiply(6.67, planet_m, 1/r, 1/r);
+  // find weight -> wei = multiply(gnu, person_m);
+    }
+    System.out.println("Bruhh");
+}
+
+//*  from user input/ 
