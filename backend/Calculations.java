76eed66725e54f01b2683522bd2381739b9a86d5
nv-ram <ramgvenu206@gmail.com>
2025-11-08 17:08:22 -0800

    Update Calculations.java
    accounts for lb weight

--- backend/Calculations.java
+++ backend/Calculations.java
@@ -16,7 +16,7 @@ public class Calculations {
   // return ;
     }
     public static double mass_p(float wei_e){
-    double person_m = wei_e/9.81 ;
+    double person_m = wei_e*.45359237/9.80665 ;
     return person_m;
 
     }
